38332f345f00af7269fcf0e3f86985185d61a026
s28600 <s28600@pjwstk.edu.pl>
2024-03-11 12:43:19 +0100

    Modyfikacja 1

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,3 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hello, World!");
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] tab = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        Console.WriteLine("Hello, World!");
+    }
+}
